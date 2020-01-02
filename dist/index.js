7110e991aa32316fb10df7e28b9cfb2a64aeabb8
KaivnD <KaivnD@hotmail.com>
2020-01-02 20:25:08 +0800

    test:deploy

--- dist/index.js
+++ dist/index.js
@@ -5845,35 +5845,55 @@ proto.requestError = async function requestError(result) {
 
 const core = __webpack_require__(470);
 const OSS = __webpack_require__(98);
+const fs = __webpack_require__(747);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  // const OSS_ID = core.getInput("oss_id");
-  // const OSS_SECRET = core.getInput("oss_secret");
-  // const BUCKET = core.getInput("bucket");
-  // const SSL = core.getInput("ssl");
-  // if (!OSS_ID || !OSS_SECRET || !BUCKET) {
-  //   throw new Error("Both OSS access key and access secret is required");
-  // }
-  // const client = new OSS({
-  //   region: "oss-accelerate", // oss-cn-shanghai
-  //   accessKeyId: OSS_KEYID,
-  //   accessKeySecret: OSS_KEYSECRET,
-  //   bucket: BUCKET,
-  //   secure: SSL
-  // });
-  console.log(
-    core
-      .getInput("files")
-      .split("\n")
-      .forEach(file => file.split("=>"))
-  );
-  console.log(
-    core
-      .getInput("dirs")
-      .split("\n")
-      .forEach(dir => dir.split("=>"))
-  );
+  const OSS_ID = core.getInput("oss_id");
+  const OSS_SECRET = core.getInput("oss_secret");
+  const BUCKET = core.getInput("bucket");
+  const SSL = core.getInput("ssl");
+
+  const prepare = str =>
+    str.split("\n").map(item => {
+      const tmp = item.trim().split("=>");
+      if (tmp.length == 2) {
+        return {
+          from: tmp[0],
+          to: tmp[1]
+        };
+      }
+    });
+
+  const files = prepare(core.getInput("files"));
+  const dirs = prepare(core.getInput("dirs"));
+
+  if (!OSS_ID || !OSS_SECRET || !BUCKET) {
+    throw new Error("Both OSS access key and access secret is required");
+  }
+
+  if (files.length === 0 || dirs.length === 0) {
+    throw new Error("files or dirs to upload is empty");
+  }
+
+  const client = new OSS({
+    region: "oss-accelerate", // oss-cn-shanghai
+    accessKeyId: OSS_KEYID,
+    accessKeySecret: OSS_KEYSECRET,
+    bucket: BUCKET,
+    secure: SSL
+  });
+
+  files.forEach(async file => {
+    if (!file) return;
+    if (!fs.existsSync(file.from)) return;
+    let stream = fs.createReadStream(file.from);
+    let result = await client.putStream(file.to, stream);
+    console.log(
+      `[${result.res.statusCode}] Put ${result.name} to OSS is ${result.res.statusMessage}`
+    );
+  });
+
+  dirs.forEach(async dir => console.log(dir));
 } catch (error) {
   core.setFailed(error.message);
 }
